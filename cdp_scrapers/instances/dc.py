6b5d9696ee4de3406e9dd14c5ab12d2972dcee18
AZIZXlaouiti <azizrocke@gmail.com>
2022-04-04 18:08:35 -0400

    adding event_minutes & matter

--- cdp_scrapers/instances/dc.py
+++ cdp_scrapers/instances/dc.py
@@ -4,6 +4,8 @@ import os
 import json
 from datetime import datetime, timedelta
 import io
+from httplib2 import Response
+from pydantic import Json
 import requests
 import fitz
 import re
@@ -14,7 +16,7 @@ from requests import request
 from cdp_scrapers.scraper_utils import IngestionModelScraper
 from cdp_scrapers.types import ContentURIs
 from urllib import request 
-
+import time
 # load_dotenv()
 TOKEN = os.environ.get('TOKEN')
 ###############################################################################
@@ -31,12 +33,35 @@ from cdp_backend.pipeline.ingestion_models import (
     Session,
     EventMinutesItem,
     MinutesItem,
+    Vote,
+    Matter
 )
-
+from time import time, sleep
 
 log = getLogger(__name__)
 
+MATTER_ADOPTED_PATTERNS = [
+    "accepted",
+    "passed",
+    "adopted",
+    "confirmed",
+]
+MATTER_IN_PROG_PATTERNS = [
+    "passed to",
+    "placed on",
+    "continued",
+    "referred",
+]
 
+MINUTE_ITEM_PASSED_PATTERNS = [
+    # NOTE: these words while have positive conotation,
+    # does not mean the legistation was passed.
+    # it indicates the item (or a report, etc.) was accepted to be discussed and voted.
+    # "accepted",
+    # "confirmed",
+    # "adopted",
+    "passed$",
+]
 # http://dc.granicus.com/viewpublisher.php?view_id=2
 COMMITTEE_VIDEO_ARCHIVES: Dict[str, str] = {
     # each page lists all meeting videos and captions for the committee
@@ -67,8 +92,54 @@ COMMITTEE_VIDEO_ARCHIVES: Dict[str, str] = {
     "Special Committee for Covid19 Pandemic Recovery":
         "http://dc.granicus.com/ViewPublisher.php?view_id=51",
 }
+_API_BASE_URL = "https://lims.dccouncil.us/api/v2/PublicData/LegislationDetails/"
+
+_HEADER = {
+        "Authorization": '1607b76f-60c7-40d6-a0ce-d8c81b65a7a1',
+        "Accept": "application/json",
+        "User-Agent": "openstates"
+    }
+class SlidingWindow:
+
+    def __init__(self, capacity, time_unit):
+        self.capacity = capacity
+        self.time_unit = time_unit
+        self.cur_time = time()
+        self.pre_count = capacity
+        self.cur_count = 0
+
+    def handle(self, packet ,action):
+
+        if (time() - self.cur_time) > self.time_unit:
+            self.cur_time = time()
+            self.pre_count = self.cur_count
+            self.cur_count = 0
+
+        ec = (self.pre_count * (self.time_unit - (time() - self.cur_time)) / self.time_unit) + self.cur_count
+
+        if (ec > self.capacity):
+            return self.drop(packet)
+
+        self.cur_count += 1
+        return self.forward(action)
 
 
+    def forward(self , action) -> json:
+        action = re.sub(r'Bill' , r'B' , action) # adapting to Bill / B legislation number for parsing request
+        if re.findall(r'((PR|B|CER|CA)\d{1,2}-\d{3})\b' , action):
+           action = re.sub(r'((PR|B|CER|CA)\d{1,2}-)\b',r'\g<1>0' , action)
+        r = requests.get(  
+            url=_API_BASE_URL + action,
+            headers=_HEADER,
+        )
+        try:
+            return r.json()
+            # print("Packet Forwarded: " + str(r.json()['actions'][0]['voteDetails']['voteResult']))
+        except: 
+            print(action)    
+
+    def drop(self , packet):
+        print("Packet Dropped: " + str(packet))
 def get_sessions(body_name: str, event_time: datetime) -> Optional[List[Session]]:
     for video_body, video_archive in COMMITTEE_VIDEO_ARCHIVES.items():
         if body_name.lower() in video_body.lower():
@@ -179,21 +250,53 @@ def get_event_materials(event_page_soup: BeautifulSoup) -> List[EventRelatedMate
         return []
 
 def get_event_minutes(agenda_uri: str) -> Optional[List[EventMinutesItem]]:
-    minutes: List[MinutesItem] = []
-    r = requests.get(agenda_uri)
-    f = io.BytesIO(r.content)
-    doc = fitz.open("pdf",f)  # open document
-    for page in doc:  # iterate the document pages (number of pages in pdf)
-        text = page.get_text().encode("utf8") # conversion to byte like object to avoid string white space conflict 
-        result =  re.findall(rb'((PR|Bill|CER|CA) \d{1,2}-\d{1,4})\b' , text)
-        for action in result :
-            minutes.append(MinutesItem(name=action[0].decode("utf8"), description=None))
-    
-    return EventMinutesItem(
-        decision=None,
-        index=0,
-        minutes_item=minutes,
+    if agenda_uri:
+        minutes_item:List[EventMinutesItem] = []
+        rate_limit = 4
+        throttle = SlidingWindow(rate_limit, 1)
+        packet = 0
+        r = requests.get(agenda_uri)
+        f = io.BytesIO(r.content)
+        doc = fitz.open("pdf",f)  # open document
+        for page in doc:  # iterate the document pages (number of pages in pdf)
+            text = page.get_text().encode("utf8") # conversion to byte like object to avoid string white space conflict 
+            result =  re.findall(rb'((PR|Bill|CER|CA) \d{1,2}-\d{1,4})\b' , text)
+            for action in result:
+                sleep(0.2)
+                minute_section = throttle.handle(packet ,re.sub(r'((PR|Bill|CER|CA) \d{1,2}-)\b',r'\g<1>0' , action[0].decode("utf8")).replace(" ", ""))
+                packet += 1 
+                
+                minutes_item.append( EventMinutesItem(
+                    minutes_item = MinutesItem(name=action[0].decode("utf8"), description=minute_section['additionalInformation']),
+                    matter = Matter(matter_type=None, name=minute_section['legislationNumber'], sponsors=None, title=minute_section['title']),
+                    decision=None,
+                    # index=0,
+                ))
+        return minutes_item        
+    else :
+        return None
+def get_matter(minute_section: json) -> Optional[Matter]:
+    r = minute_section.json()
+    matter_title = r['title']
+    doc_number = r['legislationNumber']
+    return Matter(
+            matter_type=None,
+            name=doc_number,
+            # sponsors=sponsor_list,
+            title=matter_title,
+            # result_status=result_status,
     )
+def get_votes(action: str) -> Optional[List[Vote]]:
+    try :
+        # r.raise_for_status()
+        pass
+    except (URLError, HTTPError) as e:
+        # code  = e.response.status_code
+        # log.warning(
+        #     e.response.text
+        # )
+        return None
+        
 def get_events_on_date(event_date: datetime) -> Optional[List[EventIngestionModel]]:
     try:
         # this gets us an events calendar for the month
@@ -246,18 +349,16 @@ def get_events_on_date(event_date: datetime) -> Optional[List[EventIngestionMode
         for material in event_materials:
             if "agenda" in material.name.lower():
                 agenda_uri = material.url
-                event_minutes = get_event_minutes(agenda_uri) 
                 break
         else:
             agenda_uri = None
         body = get_body(event_page_soup)
-        print(event_minutes)
         events.append(
             EventIngestionModel(
                 body=body,
                 agenda_uri=agenda_uri,
                 sessions=get_sessions(body.name, event_time),
-                # event_minutes_items= event_minutes
+                event_minutes_items= get_event_minutes(agenda_uri)
             )
         )
 
--- cdp_scrapers/instances/empty.py
+++ cdp_scrapers/instances/empty.py
@@ -6,28 +6,12 @@ from typing import List
 from unittest import result
 from datetime import datetime
 from cdp_backend.pipeline.ingestion_models import EventIngestionModel
-import dc
-
-###############################################################################
-import io
 import requests
-import fitz
+import dc
 import re
-
-# url = 'https://dccouncil.us/wp-content/uploads/2021/12/February-1-2022-Legislative-Meeting-2.pdf'
-# r = requests.get(url)
-# f = io.BytesIO(r.content)
-
-
+###############################################################################
 ingestionModel = dc.get_events_on_date(datetime(2022, 2, 1))
 print(ingestionModel)
-# doc = fitz.open("pdf",f)  # open document
-# for page in doc:  # iterate the document pages number of pages in pdf
-#     text = page.get_text().encode("utf8") # conversion to byte like object to avoid string white space confict 
-#     result =  re.findall(rb'((PR|Bill|CER|CA) \d{1,2}-\d{1,4})\b' , text)
-#     for action in result :
-#         print(action[0].decode("utf8"))
-
 def get_events(
     from_dt: datetime,
     to_dt: datetime,
